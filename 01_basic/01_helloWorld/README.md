b021f9ad3e9876f9a7f18d2650af8f8f8439c46c
dinghb-fnst <291169076@qq.com>
2016-08-28 17:55:35 +0800

    add nodejs helloworld.js

---
+++ 01_basic/01_helloWorld/README.md
@@ -0,0 +1,9 @@
+# helloworld.js
+## 运行方法  
+进入到helloworld.js文件所在的目录，输入命令：
+`node helloworl.js `
+
+如果现在后台运行，在linux系统下可以使用命令：
+`node helloworld.js &`
+
+浏览器访问 http://localhost:8124/
---
+++ 01_basic/01_helloWorld/helloworld.js
@@ -0,0 +1,19 @@
+
+// load http module.
+// Node.js 通过模块(Module)来对外提供外部程序或库
+var http = require('http');
+
+// create http server
+// offical API see http://nodejs.cn/doc/node/http.html#http_class_http_server
+// 匿名函数的参数req : 代表服务器收到的请求(http.ServerQequest)
+// 匿名函数的参数res : 代表服务器的响应(http.ServerRespone) nodejs.cn/doc/node/http.html#http_class_http_serverresponse
+http.createServer(function(req, res){
+    // content header
+    res.writeHead(200, {'content-type' : 'text/plain'});
+
+    // write message and single communication is complete
+    // http://nodejs.cn/doc/node/http.html#http_response_end_data_encoding_callback
+    res.end('Hello World!\n');
+}).listen(8124);
+
+console.log('Server is running on 8124.');
